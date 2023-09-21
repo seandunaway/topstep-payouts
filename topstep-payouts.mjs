948849886701f577e48ff0457e8bb962d9974eee
seandunaway <git@seandunaway.com>
2023-09-21 16:49:27 -0700

    tidy

--- topstep-payouts.mjs
+++ topstep-payouts.mjs
@@ -1,40 +1,45 @@
 #!/usr/bin/env node
 
-let auth = process.env.auth
+import {env} from 'node:process'
+
+let auth = env.auth
 if (!auth) throw new Error('auth')
 
 let url = 'https://discord.com/api/v9/channels/1011731142975696896/messages?limit=100'
 let referer = 'https://discord.com/channels/806986940024619039/1011731142975696896'
 
-let results = []
-let before
-
+let fetch_results = []
+let fetch_before
 while (true) {
-    let fetch_url = before ? `${url}&before=${before}` : url
+    let fetch_url = fetch_before ? `${url}&before=${fetch_before}` : url
+    let fetch_json
     try {
-        var response = await fetch(fetch_url, {headers: {'authorization': auth, 'referer': referer,}})
-        var json = await response.json()
+        let fetch_response = await fetch(fetch_url, {headers: {'authorization': auth, 'referer': referer,}})
+        fetch_json = await fetch_response.json()
     } catch (error) {
         console.error(error.message)
-        await new Promise(function (resolve) { setTimeout(resolve, 5000) })
+        await new Promise(function (resolve) { setTimeout(resolve, 10000) })
         continue
     }
 
-    if (!json.length) break
+    if (!fetch_json.length) break
 
-    for (let element of json) {
-        results.push(element)
-        before = element.id
+    for (let element of fetch_json) {
+        fetch_results.push(element)
+        fetch_before = element.id
     }
 }
 
-for (let result of results) {
-    let match = result.content.match(/(by|for) (?<name>.+?), on (?<date>.+?) for \$(?<amount>.+?)\./)
+let data = []
+for (let fetch_result of fetch_results) {
+    let match = fetch_result.content.match(/(by|for) (?<name>.+?), on (?<date>.+?) for \$(?<amount>.+?)\./)
     if (!match) continue
 
     let name = match.groups.name.toLowerCase()
     let date = new Date(match.groups.date).getTime()
     let amount = Number(match.groups.amount.replace(',', ''))
 
-    console.info(`${name},${date},${amount}`)
+    data.push({name, date, amount})
 }
+
+console.info(data)
