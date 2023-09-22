813598191b9e90f7fafb963cf5ae0c42fdb8d354
seandunaway <git@seandunaway.com>
2023-09-21 17:15:04 -0700

    before

--- topstep-payouts.mjs
+++ topstep-payouts.mjs
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import {readFile, writeFile} from 'node:fs/promises'
 import {env} from 'node:process'
 
 let auth = env.auth
@@ -8,12 +9,18 @@ if (!auth) throw new Error('auth')
 let url = 'https://discord.com/api/v9/channels/1011731142975696896/messages?limit=100'
 let referer = 'https://discord.com/channels/806986940024619039/1011731142975696896'
 
-let fetch_results = []
 let fetch_before
+try {
+    fetch_before = await readFile('./before', {encoding: 'utf8'})
+    console.info(`before ${fetch_before}`)
+} catch (error) {}
+
+let fetch_results = []
 while (true) {
     let fetch_url = fetch_before ? `${url}&before=${fetch_before}` : url
     let fetch_json
     try {
+        console.info(`fetch ${fetch_url}`)
         let fetch_response = await fetch(fetch_url, {headers: {'authorization': auth, 'referer': referer,}})
         fetch_json = await fetch_response.json()
     } catch (error) {
@@ -29,8 +36,19 @@ while (true) {
         fetch_before = element.id
     }
 }
+try {
+    writeFile('./before', fetch_before)
+    console.info(`before ${fetch_before}`)
+} catch (error) {}
+console.info(`fetched ${fetch_results.length}`)
 
 let data = []
+try {
+    let file = await readFile('./topstep-payouts.json', {encoding: 'utf8'})
+    data = JSON.parse(file)
+} catch (error) {}
+console.info(`data ${data.length}`)
+
 for (let fetch_result of fetch_results) {
     let match = fetch_result.content.match(/(by|for) (?<name>.+?), on (?<date>.+?) for \$(?<amount>.+?)\./)
     if (!match) continue
@@ -41,5 +59,6 @@ for (let fetch_result of fetch_results) {
 
     data.push({name, date, amount})
 }
+console.info(`total ${data.length}`)
 
-console.info(data)
+await writeFile('./topstep-payouts.json', JSON.stringify(data, undefined, 2))
